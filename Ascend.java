658f975feaed46d8174a5d3d997c9f172a7ff9ba
Yael Yaniv <yaelyaniv@Yaels-MacBook-Air.local>
2024-11-13 14:53:01 +0200

    Ready

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,17 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		// Creates 3 random integers between 0 and the inserted number.
+		int limit = Integer.parseInt(args[0]);
+		int num1 = (int) (Math.random() * limit);
+		int num2 = (int) (Math.random() * limit);
+		int num3 = (int) (Math.random() * limit);
+		// Determins which number in the smallest, medium or largest.
+		int small = Math.min(Math.min(num1,num2),num3);
+		int big = Math.max(Math.max(num1,num2),num3);
+		int mid = num1 + num2 + num3 - small - big;
+
+		System.out.println( num1 + " " + num2 + " " + num3);
+		System.out.println( small + " " + mid + " " + big);
 	}
 }
--- NumWords.java
+++ NumWords.java
@@ -1,6 +1,12 @@
 // Prints a given number using a hundreds, tens, and units notation.
 public class NumWords {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int num = Integer.parseInt(args[0]);
+		int oneDigit = num % 10;
+		int tenDigit = (num % 100 - oneDigit) / 10;
+		int hundredDigit = num / 100;
+		System.out.println( hundredDigit + " hundreds, " + 
+							tenDigit + " tens, and " + 
+							oneDigit +" ones.");
 	}
 }
