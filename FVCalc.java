9185c8f60edc969431e831357a16c766cfde34ab
Yael Yaniv <yaelyaniv@Yaels-MacBook-Air.local>
2024-11-13 15:40:14 +0200

    Ready3

--- FVCalc.java
+++ FVCalc.java
@@ -8,6 +8,6 @@ public class FVCalc {
 		double futureValue = currentValue * Math.pow((1+rate/100), years);
 		System.out.println("After " + years + " years, a $" + 
 							currentValue + " saved at " + rate +
-							"% will yeild $" + (int) futureValue);
+							"% will yield $" + (int) futureValue);
 	}
 }
